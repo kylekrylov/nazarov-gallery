2bd64704bf74a05582e0031fa5387287bb9e782f
Sergey Krylov <skrylov@pult.ru>
2021-03-17 18:57:18 +0300

    day3

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,41 +15,131 @@
 
 
 let portfolioSwiper = new Swiper('.portfolio__slider', {
-   slidesPerView: 4,
    spaceBetween: 40,
    loop: true,
+   slidesPerView: 4,
    breakpoints: {
-      1280: {
-         slidesPerView: 4,
-         spaceBetween: 40
+      320: {
+         slidesPerView: 1,
+         spaceBetween: 12
       },
       600: {
          slidesPerView: 2,
          spaceBetween: 20
       },
-      320: {
-         slidesPerView: 1,
-         spaceBetween: 12
+      991: {
+         slidesPerView: 3,
+         spaceBetween: 20
+      },
+      1280: {
+         slidesPerView: 4,
+         spaceBetween: 40,
       },
    }
 });
 
 let banerSwiper = new Swiper('.banner__slider', {
    slidesPerView: 1,
+   spaceBetween: 80,
+   loop: true,
+   autoHeight: true,
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+   },
+   pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+   },
+   breakpoints: {
+      1280: {
+         spaceBetween: 90
+      },
+      600: {
+         spaceBetween: 40
+      },
+      320: {
+         slidesPerView: 1,
+         spaceBetween: 20
+      },
+   },
+
+});
+
+let calendarSwiper = new Swiper('.calendar__slider', {
    spaceBetween: 40,
    loop: true,
-   // breakpoints: {
-   //    1280: {
-   //       spaceBetween: 40
-   //    },
-   //    600: {
-   //       spaceBetween: 20
-   //    },
-   //    320: {
-   //       spaceBetween: 12
-   //    },
-   // }
-});;
+   slidesPerView: 4,
+   breakpoints: {
+      320: {
+         slidesPerView: 1,
+         spaceBetween: 12
+      },
+      600: {
+         slidesPerView: 2,
+         spaceBetween: 20
+      },
+      991: {
+         slidesPerView: 3,
+         spaceBetween: 20
+      },
+      1280: {
+         slidesPerView: 4,
+         spaceBetween: 40,
+      },
+   }
+});
+
+let qualitySwiper = new Swiper('.quality__slider', {
+   slidesPerView: 3,
+   breakpoints: {
+      320: {
+         slidesPerView: 1,
+         spaceBetween: 12,
+         pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+         },
+      },
+      600: {
+         slidesPerView: 3,
+         spaceBetween: 20
+      },
+      1280: {
+         slidesPerView: 3,
+         spaceBetween: 40,
+      },
+   }
+});
+
+let reviewsSwiper = new Swiper('.reviews__slider', {
+   spaceBetween: 40,
+   loop: true,
+   slidesPerView: 2,
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+   },
+   pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+   },
+   breakpoints: {
+      320: {
+         slidesPerView: 1,
+         spaceBetween: 12
+      },
+      600: {
+         slidesPerView: 1,
+         spaceBetween: 20
+      },
+      1280: {
+         slidesPerView: 2,
+         spaceBetween: 40,
+      },
+   }
+});
+;
 
 
 
@@ -72,6 +162,7 @@ for (let smoothLink of smoothLinks) {
 //       scrollElem.style.opacity = "0";
 //    }
 // };
+
 // let timeOut;
 // function goUp() {
 //    let top = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
